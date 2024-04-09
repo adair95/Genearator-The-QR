80326e488f28c1f9efb832fdf9c7688587865536
ADAIR VILLALBA <adairvillalba95@hotmail.es>
2024-04-09 10:40:41 -0500

    Initial commit

---
+++ QR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QRCoder;
+
+namespace QR
+{
+    class Program
+    {
+        private string dato = "Aqui viene la URL del sitio o pagina a visitar";
+
+        static void Main(string[] args)
+        {
+            Program qr = new Program();
+            qr.GeneratorQR();
+        }
+        private void GeneratorQR()
+        {
+            QRCodeGenerator codeGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = codeGenerator.CreateQrCode(dato, QRCodeGenerator.ECCLevel.Q);
+
+            GetImgQrCode(qrCodeData);
+        }
+
+        private  void GetImgQrCode(QRCodeData qr)
+        {
+
+            QRCode qrCode = new QRCode(qr);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            qrCodeImage.Save("QR Code", System.Drawing.Imaging.ImageFormat.Png);
+        } 
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Generador-De-Qr
+ Generador de QR en c# .net utilizando la comuenetación de NuGet
